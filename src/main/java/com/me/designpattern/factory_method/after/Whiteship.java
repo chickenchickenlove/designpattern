9614deb2cd442263844779f2d1eccc19213cb093
ChickenchickenLove <ojt90902@naver.com>
2023-11-20 23:33:55 +0900

    git init + factory method pattern study

---
+++ src/main/java/com/me/designpattern/factory_method/after/Whiteship.java
@@ -0,0 +1,10 @@
+package com.me.designpattern.factory_method.after;
+
+public class Whiteship extends Ship{
+
+    public Whiteship(String name) {
+        setName(name);
+        setLogo("\uD83D\uDEE5️");
+        setColor("white");
+    }
+}
